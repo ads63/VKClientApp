369a9c96965b3e7a4907997ccb5616ee08c50772
ads63 <ads63@yandex.ru>
2021-08-23 19:56:37 +0300

    HW2

--- VKClientApp/Controllers/LoginViewController.swift
+++ VKClientApp/Controllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  VKClientApp
 //
 //  Created by Алексей Шинкарев on 16.08.2021.
@@ -8,16 +8,82 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet var loginLabel: UILabel!
-    @IBOutlet var passwordLabel: UILabel!
     @IBOutlet var loginTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet var scrollView: UIScrollView!
 
     @IBAction func loginButtonPressed(_ sender: Any) {
         if isValid() {
             // valid login, do something
+            print("ok")
         } else {
             // invalid login, report error
+            print("error")
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        loginTextField.text = ""
+        passwordTextField.text = ""
+
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+        navigationController?.navigationBar.isHidden = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+        navigationController?.navigationBar.isHidden = false
+    }
+
+    @objc func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue)
+            .cgRectValue
+            .size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        UIView.animate(withDuration: 1) {
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardShown" })?
+                .priority = .required
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardHide" })?
+                .priority = .defaultHigh
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        UIView.animate(withDuration: 1) {
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardShown" })?
+                .priority = .defaultHigh
+            self.scrollView.constraints
+                .first(where: { $0.identifier == "keyboardHide" })?
+                .priority = .required
+            self.view.layoutIfNeeded()
         }
     }
 
@@ -28,5 +94,12 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scrollView.addGestureRecognizer(UITapGestureRecognizer(
+            target: self,
+            action: #selector(hideKeyboard)))
+    }
+
+    @objc func hideKeyboard() {
+        scrollView.endEditing(true)
     }
 }
